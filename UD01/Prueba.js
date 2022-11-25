1120a0b0f80cb93f1998953b7346d36fddbdcca0
ale palma <alepalma211n@gmail.com>
2022-11-25 12:17:24 +0100

    subido javaScript

---
+++ UD01/Prueba.js
@@ -0,0 +1,21 @@
+
+
+var nombre = "Shaquille O'Neal";
+var altura = 216;
+
+var concatenacion = "La altura de " + nombre + " es " + altura
+
+document.write(concatenacion);
+
+function popup() {
+    alert("Hola Mundo")
+}
+
+function refrescar() {
+    window.location.reload();
+}
+
+function promter() {
+    var reply = prompt("Buenas, ¿Cuál es tu nombre? ") 
+        alert ("Es un gusto conocerte "+ reply + "!")
+}
